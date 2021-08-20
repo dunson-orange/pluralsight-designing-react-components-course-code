2dcbcd7a016abbfb5cb825836f6f01d1028a6fe3
dunson-orange <mpdunson@gmail.com>
2021-08-20 09:33:08 -0400

    Creating a Speaker Filter Context to Synchronize the toolbar and speakerlist.

---
+++ 05-context/clip-05-separating-state-management-from-context-with-custom-react-hook/src/contexts/SpeakerFilterContext.js
@@ -0,0 +1,23 @@
+import { SP } from "next/dist/next-server/lib/utils";
+import React, { createContext } from "react";
+import useSpeakerFilter from "../hooks/useSpeakerFilter";
+
+const SpeakerFilterContext = createContext();
+
+function SpeakerFilterProvider({children, startingShowSessions = false }) {
+
+  const { showSessions, setShowSessions } = useSpeakerFilter(
+    startingShowSessions
+  );
+
+  return (
+    <SpeakerFilterContext.Provider
+      value={
+        {showSessions, setShowSessions}
+      }>
+        {children}
+      </SpeakerFilterContext.Provider>
+  );
+}
+
+export {SpeakerFilterContext, SpeakerFilterProvider};
